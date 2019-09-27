64474bbfc912bf158901a8d7ff67c07559e41132
steviemul <stephen.mulrennan@ntlworld.com>
2019-09-27 22:15:52 +0100

    Initial commit.

---
+++ src/data/components.ts
@@ -0,0 +1,71 @@
+import * as path from 'path';
+import * as fs from 'fs';
+
+const APPS = 'apps';
+const PACKAGES = 'packages';
+const OCC = '.occ';
+const APP_JSON = 'app.json';
+
+const getCompoentInstances = (appJsonLocation: string) => {
+
+  const appPath = path.resolve(appJsonLocation, '..', '..');
+  const instanceLocation = path.join(appPath, 'assets', 'components');
+
+  if (fs.existsSync(instanceLocation)) {
+    return fs.readdirSync(instanceLocation);
+  }
+
+  return [];
+};
+
+const readJson = (location: string) => {
+  if (fs.existsSync(location)) {
+    const json: string = fs.readFileSync(location, 'utf8');
+
+    return JSON.parse(json);
+  }
+};
+
+const getAppRoot = (fileLocation: string) => {
+
+  const parts = fileLocation.split(path.sep);
+
+  for (let i = parts.length - 1; i > 0; i--) {
+
+    if (parts[i - 1] === APPS) {
+      if (i > 1 && parts[i - 2] === PACKAGES) {
+        return parts.splice(0, i + 1).join(path.sep);
+      }
+    }
+  }
+};
+
+const getComponentsForContainingApp = (fileLocation: string, includeInstances: boolean = false) => {
+
+  const appRoot = getAppRoot(fileLocation);
+
+  if (appRoot) {
+    const appJsonLocation = path.join(appRoot, OCC, APP_JSON);
+
+    if (fs.existsSync(appJsonLocation)) {
+      const appJson = readJson(appJsonLocation);
+
+      if (includeInstances) {
+        const instances = getCompoentInstances(appJsonLocation);
+
+        for (let instance of instances) {
+          appJson.components[instance] = {};
+        }
+      }
+
+      return appJson;
+    }
+  }
+  
+  return {};
+};
+
+export {
+  getAppRoot,
+  getComponentsForContainingApp
+};
---
+++ src/parser/index.ts
@@ -0,0 +1,54 @@
+import * as vscode from "vscode";
+
+const COLON = ':';
+const QUOTE = '"';
+const MAX_DISTANCE  = 200;
+
+const getNearestProperty = (document: vscode.TextDocument, startPosition: number) => {
+
+  let distance: number = 0;
+  let approachingProperty: boolean = false;
+  let inPropertyKey: boolean = false;
+  let property: string = '';
+
+  for (let i=startPosition;i>=0;i--) {
+    const line = document.lineAt(i).text;
+    
+    for (let j=line.length;j>=0;j--) {
+      if (distance++ >= MAX_DISTANCE) {
+        return;
+      }
+
+      const ch = line.charAt(j);
+
+      if (inPropertyKey === true) {
+        if (ch === QUOTE) {
+          return property.split("").reverse().join("");
+        }
+        else {
+          property += ch;
+        }
+      }
+      else if (approachingProperty) {
+        if (ch === QUOTE) {
+          inPropertyKey = true;
+          approachingProperty = false;
+        }
+      }
+      else if (ch === COLON) {
+        if (approachingProperty) {
+          console.log('Invalid state searching for property');
+          return;
+        }
+        else {
+          approachingProperty = true;
+        }
+      }
+    }
+  }
+  
+};
+
+export {
+  getNearestProperty
+};
